2d49e8f8b8a627b481267b9763d14ec0ed50140d
Roman <destabiliseus@gmail.com>
2022-12-08 01:12:31 +0800

    docs: add jsdoc to useTaskExpire hook

--- src/hooks/useTaskExpire.ts
+++ src/hooks/useTaskExpire.ts
@@ -3,6 +3,11 @@ import { useEffect, useRef } from 'react';
 
 import { Task } from '@/types/task';
 
+/**
+ * Hook that checks `Task` due date and run callback when it's expire.
+ * @param task - Task that should be watched.
+ * @param callback - A function that should be invoked when task will be expired.
+ */
 export const useTaskExpire = (task: Task, callback: () => void) => {
   if (!callback) {
     throw new Error('[useTaskExpire] Please provide callback function.');
