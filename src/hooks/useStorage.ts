7fe13516cb6086527f128b0d5debd92b4b51cc35
Roman <destabiliseus@gmail.com>
2022-12-08 01:09:45 +0800

    docs: add jsdoc to useStorage hook

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -2,7 +2,20 @@ import { getDownloadURL, ref, uploadBytes, UploadResult } from 'firebase/storage
 
 import { storage } from '@/services/firebase';
 
+/**
+ * Hook that utilizes `Storage` files.
+ * 
+ @returns A number of functions to manage `Storage` files.
+ */
 export const useStorage = ({ path } = { path: '/files/' }) => {
+  /**
+   * The function uploads file in `Storage`.
+   *
+   * @param file - Data to upload.
+   * In our specific case we will upload an object that retrieved from FileList.
+   * @async
+   * @returns A `Promise` that will be resolved with storage upload data.
+   */
   const upload = (file: File): Promise<UploadResult> => {
     const storageRef = ref(storage, `${path}${file.name}`);
 
@@ -13,6 +26,11 @@ export const useStorage = ({ path } = { path: '/files/' }) => {
     return uploadBytes(storageRef, file, metadata);
   };
 
+  /**
+   *
+   * @param path - Path to a file on the storage.
+   * @returns A `Promise` that will be resolved with a string.
+   */
   const getFileDownloadURL = (path: string): Promise<string> => {
     return getDownloadURL(ref(storage, path));
   };
