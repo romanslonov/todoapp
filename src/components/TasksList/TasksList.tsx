d6b0e386e18532858549f7bdb08b868be19178d9
Roman <destabiliseus@gmail.com>
2022-12-08 12:11:44 +0800

    feat: add sort by status and date of tasks

--- src/components/TasksList/TasksList.tsx
+++ src/components/TasksList/TasksList.tsx
@@ -1,16 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Task from '@/components/Task/Task';
 import TaskForm from '@/components/TaskForm';
 import { useTasks } from '@/hooks/useTasks';
+import type { Task as TaskType } from '@/types/task';
 
 import styles from './styles.module.less';
 
 export default function TasksList() {
   const { fetch, tasks, create, changeStatus, remove, update } = useTasks();
 
-  const activeTasksList = tasks.filter((task) => task.status !== 'completed');
-  const completedTasksList = tasks.filter((task) => task.status === 'completed');
+  const [activeTasksList, setActiveTasksList] = useState<TaskType[]>([]);
+  const [completedTasksList, setCompletedTasksList] = useState<TaskType[]>([]);
+
+  useEffect(() => {
+    if (tasks.length) {
+      setActiveTasksList(
+        [...tasks]
+          .filter((task) => task.status !== 'completed')
+          .sort(
+            (a, b) =>
+              a.status.localeCompare(b.status) ||
+              b.created_at.getTime() - a.created_at.getTime(),
+          ),
+      );
+
+      setCompletedTasksList(
+        [...tasks]
+          .filter((task) => task.status === 'completed')
+          .sort((a, b) => b.created_at.getTime() - a.created_at.getTime()),
+      );
+    }
+  }, [tasks]);
 
   useEffect(() => {
     fetch();
@@ -43,7 +64,7 @@ export default function TasksList() {
           <ul className={styles.list}>
             {completedTasksList.map((task) => (
               <li key={task.id}>
-                <Task onChangeStatus={changeStatus} task={task} />
+                <Task onChangeStatus={changeStatus} onRemove={remove} task={task} />
               </li>
             ))}
           </ul>
